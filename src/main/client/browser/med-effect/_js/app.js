ebbd44c28dd8d0ce391f90661cc784d659b9f427
Wim Ibes <wimibes@gmail.com>
2015-07-02 08:38:48 -0700

    Update to search page

--- src/main/client/browser/med-effect/_js/app.js
+++ src/main/client/browser/med-effect/_js/app.js
@@ -12,61 +12,82 @@ function paramSerializer(params) {
 		&count=patient.drug.medicinalproduct.exact
 */
 angular.module('medInfoApp', []).controller('MedsearchController', ['$scope', '$http', '$templateCache',
-	function($scope, $http, $templateCache) {
-		var medList = this;
+		function($scope, $http, $templateCache) {
+			var medList = this;
 
-		$scope.method = 'GET';
-		$scope.url = 'https://api.fda.gov/drug/event.json';
-		
-		$scope.keywords = 'ibuprofin';
-
-		medList.meds = [ {term: 'ibuprofin', count: 100}
-			, {term: 'tylenol', count: 70}
-		];
-		medList.meds = [];
-
-		$scope.fetch = function() {
-			var text = $scope.keywords;
-
-			var requestConfig = {
-				method: $scope.method
-				, url: $scope.url
-				, params: {
-						api_key: 'LZgWcrkV7bemrG9i8sKDE8GbWWAUbbOzIRTIOuxU'
-						, search: '(patient.drug.medicinalproduct:"' + text + '") AND serious:1'
-						, count: 'patient.drug.medicinalproduct.exact'
-						, limit: 100
-				}
-				, cache: $templateCache
-			}
-
-			$scope.code = null;
-			$scope.response = null;
+			$scope.method = 'GET';
+			$scope.url = 'https://api.fda.gov/drug/event.json';
+			
+			$scope.keywords = 'ibuprofen';
 
 			medList.meds = [];
 
-			try {
-				$scope.requesturl = $scope.url + '?' + paramSerializer(requestConfig.params);
-			} catch (e) {
-				alert('oops: ' + e.message);
-			}
+			$scope.fetch = function() {
+				var text = $scope.keywords;
+				var filters = [];
 
-			$http(requestConfig).
-				success(function(data, status) {
-					$scope.results = data.results.length;
+				if ($scope.filterMedicationName) {
+					filters.push({ field: $scope.medicationField, value: $scope.medicationName });
+				}
+				if ($scope.filterAdministrationRoute) {
+					filters.push({ field: 'patient.drug.drugadministrationroute', value: $scope.administrationRoute });
+				}
+				if ($scope.filterDosage) {
+					filters.push({ field: 'patient.drug.drugcumulativedosageunit', value: $scope.dosageUnit });
+					filters.push({ field: 'patient.drug.drugcumulativedosagenumb', value:  });
+				}
+				if ($scope.filterSince) {
+					filters.push({ field: , value:  });
+				}
 
-					$scope.status = status;
-					$scope.data = data;
+				return;
 
-					angular.forEach(data.results, function(result) {
-						medList.meds.push({term: result.term, count: result.count});
-					});
-				}).
-				error(function(data, status) {
-					$scope.data = data || "Request failed";
-					$scope.status = status;
-			});
-		};
+				var requestConfig = {
+					method: $scope.method
+					, url: $scope.url
+					, params: {
+							search: '(patient.drug.openfda.generic_name:' + text + ')'
+							, count: 'patient.drug.openfda.generic_name'
+							, limit: 100
+							, api_key: 'LZgWcrkV7bemrG9i8sKDE8GbWWAUbbOzIRTIOuxU'
+					}
+					, cache: $templateCache
+				}
 
-	}]);
+				$scope.code = null;
+				$scope.response = null;
+
+				medList.meds = [];
+
+				try {
+					$scope.requesturl = $scope.url + '?' + paramSerializer(requestConfig.params);
+				} catch (e) {
+					alert('oops: ' + e.message);
+				}
+
+				$http(requestConfig).
+					success(function(data, status) {
+						$scope.results = data.results.length;
+
+						$scope.status = status;
+						$scope.data = data;
+
+						angular.forEach(data.results, function(result) {
+							medList.meds.push({term: result.term, count: result.count});
+						});
+					}).
+					error(function(data, status) {
+						$scope.data = data || "Request failed";
+						$scope.status = status;
+				});
+			};
+
+		}
+	])
+	.controller('EventtypeController', ['$scope', '$http', '$templateCache',
+		function($scope, $http, $templateCache) {
+			
+		}
+	]);
+
 
